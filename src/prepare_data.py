e1d5f0229798e87254daab4171a432e36dce4b3b
Kor4yz <147404339+Kor4yz@users.noreply.github.com>
2025-08-17 00:09:49 +0300

    Delete src/prepare_data.py

--- src/prepare_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load(path='../data/transactions_sample.csv'):
-    return pd.read_csv(path, parse_dates=['order_date'])
-
-if __name__ == '__main__':
-    df = load()
-    print(df.head())
